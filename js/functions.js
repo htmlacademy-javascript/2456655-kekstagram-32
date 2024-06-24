ba2f26b11851097852b8b3a2c0954a8423ce9640
Oksana Lanovenko <okslan@gmail.com>
2024-06-24 19:32:43 +0400

    Попытка выполнить ДЗ (2)

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,22 @@
-const string = 'Первая строка в коде';
-const countStringLength = function (string) {
-  if(string < 20) {
+const str = 'Первая строка в коде';
+console.log(str.length);
+
+const findStringLength = function(str, 20) {
+  if (str.length <= 20) {
     return true;
   }
-};
+}
+
+const str = 'Первая строка в коде';
+const findStringLength = function(str, 18) {
+  if (str.length === 18) {
+    return true;
+  }
+}
+
+const str = 'Первая строка в коде';
+const findStringLength = function(str, 10) {
+  if (str.length >= 10) {
+    return false;
+  }
+}
