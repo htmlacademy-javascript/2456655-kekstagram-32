e58e1fed6b0b9c9fb37f183e4dce5062c3279143
Oksana Lanovenko <okslan@gmail.com>
2024-06-27 14:35:25 +0400

    Попытка выполнить ДЗ (6)

--- js/functions.js
+++ js/functions.js
@@ -2,4 +2,11 @@ const isStringShorter20 = (string, length20) => string.length <= length20;
 const isStringLength18 = (string, length18) => string.length === length18;
 const isStringLonger10 = (string, length10) => string.length >= length10;
 
-void (isStringShorter20, isStringLength18, isStringLonger10);
+const isPalindrom = (string) => {
+  const initialString = string.toLowerCase().replaceAll(' ', '');
+  const reversedString = initialString.split('').reverse().join('');
+
+  return initialString === reversedString;
+};
+
+void (isStringShorter20, isStringLength18, isStringLonger10, isPalindrom);
