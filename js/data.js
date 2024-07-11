15d928165fb8701409c720ff74abdd0c52468d4e
Oksana Lanovenko <okslan@gmail.com>
2024-07-11 19:28:09 +0400

    ДЗ 5.14 (2)

--- js/data.js
+++ js/data.js
@@ -1,3 +1,5 @@
+import {getRandomInteger, getRandomArrayElement, createIdGenerator} from './util.js';
+
 const PICTURE_COUNT = 25;
 const LIKES_MIN = 15;
 const LIKES_MAX = 200;
@@ -44,24 +46,6 @@ const ARRAY_NAMES = [
   'Игорь'
 ];
 
-const getRandomInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-const getRandomArrayElement = (items) =>
-  items[getRandomInteger(0, items.length - 1)];
-
-const createIdGenerator = () => {
-  let numId = 0;
-
-  return() => {
-    numId += 1;
-    return numId;
-  };
-};
 
 const getRandomId = createIdGenerator();
 
@@ -93,6 +77,5 @@ const getPictures = () => Array.from(
   (_, index) => createPicture(index + 1),
 );
 
-getPictures();
 
 export {getPictures};
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {getPictures} from './data.js';
+import { getPictures } from './data.js';
 
 getPictures();
 
