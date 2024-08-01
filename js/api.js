e3ae470b1e9e1ce0f6482a91dfffa47a15886786
Oksana Lanovenko <okslan@gmail.com>
2024-08-01 21:41:19 +0400

    ДЗ 14.11 (2)

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://32.javascript.htmlacademy.pro/kekstagram';
+const BASE_URL = 'https://32.javascript.pages.academy/kekstagram';
 const Route = {
   GET_DATA: '/data',
   SEND_DATA: '/',
@@ -9,20 +9,20 @@ const Method = {
   POST: 'POST',
 };
 
-const load = (route, method = Method.GET, body : null) =>
+const load = (route, method = Method.GET, body = null) =>
   fetch(`${BASE_URL}${route}`, {method, body})
-.then((response) => {
-  if(!response.ok) {
-    throw new Error(`${response.status}: ${response.statusText}`);
-  }
-  return response.json();
-})
-.catch((err) => {
-  throw new Error(err.message);
-});
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error(`${response.status}: ${response.statusText}`);
+      }
+      return response.json();
+    })
+    .catch((err) => {
+      throw new Error(err.message);
+    });
 
-const getData = () => loadConfigFromFile(Route.GET_DATA);
+const getData = () => load(Route.GET_DATA);
 
-const sendData = (body) => loadConfigFromFile(Route.SEND_DATA, Method.POST, body);
+const sendData = (body) => load(Route.SEND_DATA, Method.POST, body);
 
 export {getData, sendData};
